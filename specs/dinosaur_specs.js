fd0df0ba36a65e7fd952f3da80c5de780e8296a0
Christiebelle <cslester@outlook.com>
2018-04-03 19:47:02 +0100

    beforeeach set up in dino and park specs

---
+++ specs/dinosaur_specs.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+var Dinosaur = require('../dinosaur.js')
+
+describe ('Park', function(){
+
+  var dino1, dino2, dino3, dino4;
+
+  beforeEach(function(){
+
+  });
+});
---
+++ specs/park_specs.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var Park = require('../park.js');
+var Dinosaur = require('../dinosaur.js')
+
+describe ('Park', function(){
+
+  var park, dino1, dino2, dino3, dino4;
+
+  beforeEach(function(){
+
+  });
+});
