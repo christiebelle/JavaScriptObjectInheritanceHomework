625f477cb748e13988a65701afbaa235be15dff1
Christiebelle <cslester@outlook.com>
2018-04-03 20:19:35 +0100

    dinosaur tests passing

--- specs/dinosaur_specs.js
+++ specs/dinosaur_specs.js
@@ -6,10 +6,10 @@ describe ('Park', function(){
   var dino1, dino2, dino3, dino4;
 
   beforeEach(function(){
-    var dino1 = new Dinosaur("Blue", "Velociraptor", 3);
-    var dino2 = new Dinosaur("Delta", "Velociraptor", 2);
-    var dino3 = new Dinosaur("Diggle", "T-Rex", 4);
-    var dino4 = new Dinosaur("Cisco", "Triceratops", 3);
+    dino1 = new Dinosaur("Blue", "Velociraptor", 3);
+    dino2 = new Dinosaur("Delta", "Velociraptor", 2);
+    dino3 = new Dinosaur("Diggle", "T-Rex", 4);
+    dino4 = new Dinosaur("Cisco", "Triceratops", 3);
   });
 
   it ('should have a name', function(){
